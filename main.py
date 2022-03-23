2b4040834769118075725ade73166df6cde2bd9c
Subham-RKB <subhamchapagain45@gmail.com>
2022-03-23 14:50:36 +0530

    Made Window

---
+++ main.py
@@ -0,0 +1,10 @@
+import os
+from tkinter import *
+from tkinter import messagebox
+#creating the application main window
+window = Tk()
+window.title("AI Poered Note Maker")
+window.geometry("600x600")
+window.configure(bg="#000000")
+
+window.mainloop()
