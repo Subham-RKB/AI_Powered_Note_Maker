6798d4de8d41fecc733b458984462dc5b0e9dfde
Gunjan Raj Tiwari <im.gunjan1@gmail.com>
2022-03-24 19:06:13 +0530

    integration 🔥

--- abstractive.py
+++ abstractive.py
@@ -30,16 +30,16 @@ def summarize(src_text):
     print("Length of summary: ", len(result.split()))
     return result
 
-text = """John McCarthy was born in Boston, Massachusetts, on September 4, 1927, to an Irish immigrant father and a Lithuanian Jewish immigrant mother,[4] John Patrick and Ida (Glatt) McCarthy. The family was obliged to relocate frequently during the Great Depression, until McCarthy's father found work as an organizer for the Amalgamated Clothing Workers in Los Angeles, California. His father came from Cromane, a small fishing village in County Kerry, Ireland.[5] His mother died in 1957.[6]
+# text = """John McCarthy was born in Boston, Massachusetts, on September 4, 1927, to an Irish immigrant father and a Lithuanian Jewish immigrant mother,[4] John Patrick and Ida (Glatt) McCarthy. The family was obliged to relocate frequently during the Great Depression, until McCarthy's father found work as an organizer for the Amalgamated Clothing Workers in Los Angeles, California. His father came from Cromane, a small fishing village in County Kerry, Ireland.[5] His mother died in 1957.[6]
 
-Both parents were active members of the Communist Party during the 1930s and encouraged learning and critical thinking. Before he attended high school, he got interested in science by reading a translation of a Russian popular science book for children, called 100,000 Whys.[7] John was fluent in the Russian language and made friends with Russian scientists during multiple trips to the Soviet Union but he became disillusioned after making visits to the Soviet Bloc which lead to him becoming a conservative Republican.[8]
+# Both parents were active members of the Communist Party during the 1930s and encouraged learning and critical thinking. Before he attended high school, he got interested in science by reading a translation of a Russian popular science book for children, called 100,000 Whys.[7] John was fluent in the Russian language and made friends with Russian scientists during multiple trips to the Soviet Union but he became disillusioned after making visits to the Soviet Bloc which lead to him becoming a conservative Republican.[8]
 
-McCarthy graduated from Belmont High School two years early.[9] McCarthy was accepted into Caltech in 1944.
+# McCarthy graduated from Belmont High School two years early.[9] McCarthy was accepted into Caltech in 1944.
 
-McCarthy showed an early aptitude for mathematics; during his teens he taught himself college mathematics by studying the textbooks used at the nearby California Institute of Technology (Caltech). As a result, he was able to skip the first two years of mathematics at Caltech.[10] McCarthy was suspended from Caltech for failure to attend physical education courses.[11] He then served in the US Army and was readmitted, receiving a B.S. in mathematics in 1948.[12]
+# McCarthy showed an early aptitude for mathematics; during his teens he taught himself college mathematics by studying the textbooks used at the nearby California Institute of Technology (Caltech). As a result, he was able to skip the first two years of mathematics at Caltech.[10] McCarthy was suspended from Caltech for failure to attend physical education courses.[11] He then served in the US Army and was readmitted, receiving a B.S. in mathematics in 1948.[12]
 
-It was at Caltech that he attended a lecture by John von Neumann that inspired his future endeavors.
+# It was at Caltech that he attended a lecture by John von Neumann that inspired his future endeavors.
 
-McCarthy initially completed graduate studies at Caltech before moving to Princeton University. He received a Ph.D. in mathematics from Princeton in 1951 after completing a doctoral dissertation, titled "Projection operators and partial differential equations", under the supervision of Donald C. Spencer.[13]"""
+# McCarthy initially completed graduate studies at Caltech before moving to Princeton University. He received a Ph.D. in mathematics from Princeton in 1951 after completing a doctoral dissertation, titled "Projection operators and partial differential equations", under the supervision of Donald C. Spencer.[13]"""
     
-print(summarize(text))
+# print(summarize(text))
--- audioPage.py
+++ audioPage.py
@@ -1,4 +1,7 @@
 from tkinter import *
+from tkinter import filedialog
+import speech_recognition as sr
+import abstractive
 
 from templates import Button_Template
 import landingPage
@@ -8,17 +11,64 @@ class AudioPage:
     def __init__(self, window):
         self.audioPage = Frame(window)
         self.window = window
+        self.fileTitle = ""
         
-
     def show(self):
         self.audioPage.pack(side = "top", fill = "both", expand = True)
-        l1 = Label(self.audioPage, text="MY Audios", font=(
-                "Bernard MT Condensed", 32), bg="#ffffff", fg="black")
+        l1 = Label(self.audioPage, text="Upload Audio", font=(
+                "Bernard MT Condensed", 30), bg="#ffffff", fg="black")
         l1.place(x=230, y=30)
-        Button_Template("Back","",200,250,2,30,self.LandingPage,self.audioPage)
+
+        Button_Template("Back","",4,4,1,4,self.LandingPage,self.audioPage)
+
+        Button_Template("Upload","",200,100,1,5,self.uploadAudio,self.audioPage)
+
+        self.l3 = Label(self.audioPage, text=self.fileTitle, font=("Lora", 12), bg="#ffffff", fg="black")
+        
+
+    def uploadAudio(self):
+        self.filePath = filedialog.askopenfilename(initialdir="/audioFiles", title="Select a File", filetypes = (("wav files", "*.wav"), ("All files", "*.*")))
+
+        self.fileTitle = self.filePath.split('/')[-1]
+        
+        self.l3["text"] = self.fileTitle
+        self.l3.place_forget()
+        self.l3.place(x=200, y=200)
+
+        Button_Template("Summarize","",200,150,1,10,self.Summarize,self.audioPage)
+
+    def Summarize(self):
+        r = sr.Recognizer()
+
+        self.l3["text"] = "Fetching Audio File ..."
+        self.l3.place_forget()
+        self.l3.place(x=200, y=200)
+
+        audioFile = sr.AudioFile(self.filePath)
+
+        with audioFile as source:
+            audio = r.record(source)
+
+        self.l3["text"] = "Transcribing Audio ..."
+        self.l3.place_forget()
+        self.l3.place(x=200, y=200)
+
+        transcript = r.recognize_google(audio)
+
+        self.l3["text"] = "Summarizing ..."
+        self.l3.place_forget()
+        self.l3.place(x=200, y=200)
+
+        summary = abstractive.summarize(transcript)
+
+        print(summary)
+
+
+        
 
     def destroy(self):
         self.audioPage.destroy()
+
     def LandingPage(self):
         pg = landingPage.LandingPage(self.window)
         pg.show()
