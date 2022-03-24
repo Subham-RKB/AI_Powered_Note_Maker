62ac1ef43e3d9427f69d5cabb4862de797677d07
roatt-tilma <roshanbhattmnr@gmail.com>
2022-03-24 23:43:25 +0530

    backend flask

--- Flask/app.py
+++ Flask/app.py
@@ -1,18 +1,68 @@
 from flask import Flask
+from flask import *
+import speech_recognition as sr
+from transformers import PegasusForConditionalGeneration, PegasusTokenizer
+import torch
 
 app = Flask(__name__)
 
+def convertToText(filename):
+    r = sr.Recognizer()
+
+    audioFile = sr.AudioFile("./audioFiles/" + filename)
+
+    with audioFile as source:
+        audio = r.record(source)
+
+    transcript = r.recognize_google(audio)
+
+    return transcript
+
+
+def generateSummary(transcript):
+    text = transcript.split(' ')
+
+    model_name = 'google/pegasus-large'
+    tokenizer = PegasusTokenizer.from_pretrained(model_name)
+    model = PegasusForConditionalGeneration.from_pretrained(model_name)
+
+    result = ""
+    textSize = len(text)
+    cnt = int(textSize / 200)
+    for i in range(cnt + 1):
+        j = i*100
+        cur = ""
+
+        while(j < min((i+1) * 100, textSize)):
+            cur += text[j] + ' '
+            j += 1
+
+        tokens = tokenizer(cur, truncation=True, padding="longest", return_tensors="pt")
+        summary = model.generate(**tokens)
+        summarized_text = tokenizer.decode(summary[0])
+        result += summarized_text + ' '
+
+    return result
+
+
+
 @app.route('/')
 def Landing():
-    return "Landing Page"
+    return render_template('index.html')
 
-@app.route('/transcribe')
-def Trancsribe():
-    return "Transcribing"
-
-@app.route('/summarize')
+@app.route('/summarize', methods = ['POST'])
 def Summarize():
-    return "Summarizing"
+    if request.method == 'POST':  
+        f = request.files['audioFile']  
+        f.save("./audioFiles/" + f.filename)  
+
+        transcript = convertToText(f.filename)
+
+        summary = generateSummary(transcript)
+
+        return summary
+
+        
 
 if __name__ == "__main__":
     app.run()
